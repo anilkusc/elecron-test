196e3b8045e49b4022d23a6b746bdd7e7de5302f
anilkusc <anilkuscu95@gmail.com>
2022-09-11 17:58:44 +0300

    process communication

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  ipcMain
+} = require("electron");
+const path = require("path");
+const fs = require("fs");
 
-const path = require('path')
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-        webSecurity: false,
-        nodeIntegration: true
-      },
-    })
-    ipcMain.handle('ping', () => 'pong')
-    win.loadFile('index.html')
-    win.openDevTools()
-  }
+async function createWindow() {
 
-app.whenReady().then(() => {
-  createWindow()
-})
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+      preload: path.join(__dirname, "preload.js") // use a preload script
+    }
+  });
 
-ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log( arg );
-    
-    // send message to index.html
-    //event.sender.send('asynchronous-reply', 'hello' );
-    });
+  // Load app
+  win.loadFile(path.join(__dirname, "index.html"));
+  win.openDevTools()
+  // rest of code..
+}
+
+app.on("ready", createWindow);
+
+ipcMain.on("toMain", (event, args) => {
+  console.log(args)
+  win.webContents.send("fromMain", args["username"]);
+});
--- preload.js
+++ preload.js
@@ -1,9 +1,25 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const {
+  contextBridge,
+  ipcRenderer
+} = require("electron");
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping'),
-  // we can also expose variables, not just functions
-})
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  "api", {
+      send: (channel, data) => {
+          // whitelist channels
+          let validChannels = ["toMain"];
+          if (validChannels.includes(channel)) {
+              ipcRenderer.send(channel, data);
+          }
+      },
+      receive: (channel, func) => {
+          let validChannels = ["fromMain"];
+          if (validChannels.includes(channel)) {
+              // Deliberately strip event as it includes `sender` 
+              ipcRenderer.on(channel, (event, ...args) => func(...args));
+          }
+      }
+  }
+);
